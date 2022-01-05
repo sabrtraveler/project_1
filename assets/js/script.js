123611e4ce9a7ea31e4090fe1aa45266a7955ddf
veeditparikh2211 <veeditparikh2211@gmail.com>
2022-01-05 18:10:44 -0500

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,58 +7,58 @@ const list = document.querySelector(".ajax-section .cities");
 const apiKey = "3a5dcccabb31635037afffcfa07050bc";
 
 form.addEventListener("submit", e => {
-  e.preventDefault();
-  let inputVal = input.value;
+    e.preventDefault();
+    let inputVal = input.value;
 
-  //check if there's already a city
-  const listItems = list.querySelectorAll(".ajax-section .city");
-  const listItemsArray = Array.from(listItems);
+    //check if there's already a city
+    const listItems = list.querySelectorAll(".ajax-section .city");
+    const listItemsArray = Array.from(listItems);
 
-  if (listItemsArray.length > 0) {
-    const filteredArray = listItemsArray.filter(el => {
-      let content = "";
-      //athens,gr
-      if (inputVal.includes(",")) {
-        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
-        if (inputVal.split(",")[1].length > 2) {
-          inputVal = inputVal.split(",")[0];
-          content = el
-            .querySelector(".city-name span")
-            .textContent.toLowerCase();
-        } else {
-          content = el.querySelector(".city-name").dataset.name.toLowerCase();
-        }
-      } else {
-        //athens
-        content = el.querySelector(".city-name span").textContent.toLowerCase();
-      }
-      return content == inputVal.toLowerCase();
-    });
+    if (listItemsArray.length > 0) {
+        const filteredArray = listItemsArray.filter(el => {
+            let content = "";
+            //athens,gr
+            if (inputVal.includes(",")) {
+                //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+                if (inputVal.split(",")[1].length > 2) {
+                    inputVal = inputVal.split(",")[0];
+                    content = el
+                        .querySelector(".city-name span")
+                        .textContent.toLowerCase();
+                } else {
+                    content = el.querySelector(".city-name").dataset.name.toLowerCase();
+                }
+            } else {
+                //athens
+                content = el.querySelector(".city-name span").textContent.toLowerCase();
+            }
+            return content == inputVal.toLowerCase();
+        });
 
-    if (filteredArray.length > 0) {
-      msg.textContent = `You already know the weather for ${
+        if (filteredArray.length > 0) {
+            msg.textContent = `You already know the weather for ${
         filteredArray[0].querySelector(".city-name span").textContent
       } ...otherwise be more specific by providing the country code as well 😉`;
-      form.reset();
-      input.focus();
-      return;
+            form.reset();
+            input.focus();
+            return;
+        }
     }
-  }
 
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    //ajax here
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const { main, name, sys, weather } = data;
+            const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
         weather[0]["icon"]
       }.svg`;
 
-      const li = document.createElement("li");
-      li.classList.add("city");
-      const markup = `
+            const li = document.createElement("li");
+            li.classList.add("city");
+            const markup = `
         <h2 class="city-name" data-name="${name},${sys.country}">
           <span>${name}</span>
           <sup>${sys.country}</sup>
@@ -71,14 +71,49 @@ form.addEventListener("submit", e => {
           <figcaption>${weather[0]["description"]}</figcaption>
         </figure>
       `;
-      li.innerHTML = markup;
-      list.appendChild(li);
-    })
-    .catch(() => {
-      msg.textContent = "Please search for a valid city 😩";
+            li.innerHTML = markup;
+            list.appendChild(li);
+        })
+        .catch(() => {
+            msg.textContent = "Please search for a valid city 😩";
+        });
+
+    msg.textContent = "";
+    form.reset();
+    input.focus();
+});
+
+$(document).ready(function() {
+
+
+    var API_KEY = "AIzaSyCidAtnKLK0NmfyPCf9auAA459rZytLyAk"
+
+    var video = "";
+
+    $("form").submit(function(event) {
+        event.preventDefault();
+
+        var search = $("#search").val();
+        videoSearch(API_KEY, search, 5);
     });
 
-  msg.textContent = "";
-  form.reset();
-  input.focus();
-});
+    function videoSearch(key, search, maxResults) {
+        $("#videos").empty();
+
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&q=" + search + "&type=video&part=snippet&&maxResults" + maxResults, function(data) {
+            console.log(data),
+
+                data.items.forEach(item => {
+
+                    video = `
+                    <iframe width="200" height="200"
+                    src="https://www.youtube.com/embed/${item.id.videoId}"
+                    frameborder="0" allowfullscreen></iframe> 
+                      `
+
+                    $("#videos").append(video);
+                });
+        })
+    }
+
+})
