0ab9d1fd27d0d0017ef2afc9f690d16b7a8ff2b6
sabrtraveler <sabrtraveler@gmail.com>
2022-01-14 22:59:54 -0500

    new message

--- README.md
+++ README.md
@@ -4,14 +4,18 @@
 
 1. Robert World
 2. Veedit Parikh
-3. Hoban Assoweh
-4. Helody
+3. Habon Assoweh
+4. Helody Francois
 
 # Description
 
-Corona Virus is spreading wilder than ever. Now is 2022 and we need to keep track of this pandamic. We created a Corona Virus tracker & Top news headlines from New York times.
+Github page: https://sabrtraveler.github.io/project_1/
+
+The Covid pandemic has been a terrible globalexperience. The Covid Watch app was designed toget the latest Covid-19 data in the United States. Inaddition, you will also be able to view the latest newsheadlines from across the globe. This data isautomatically updated every 12hrs.
 
 2 Apis
-
 Covid tracker api - https://github.com/ExpDev07/coronavirus-tracker-api <br>
 New York Times news - https://developer.nytimes.com/apis
+
+The following images shows the web application's appearance and functionality:<br><br>
+<img src="./assets/images/1.png" style="width: 50%; max-width: 50%;">
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,19 @@ window.onload = function() {
     nytimes();
 }
 
-
+// function to get Covid status by fetching from api
 function getCovidStats() {
     fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/225')
         .then(function(resp) { return resp.json() })
         .then(function(data) {
 
-            let population = data.location.country_population;
-            let update = data.location.last_updated;
-            let confirmedCases = data.location.latest.confirmed;
-            let deaths = data.location.latest.deaths;
+            // set variables for certain data properties
+            var population = data.location.country_population;
+            var update = data.location.last_updated;
+            var confirmedCases = data.location.latest.confirmed;
+            var deaths = data.location.latest.deaths;
 
-
+            // embed data onto the web page
             document.getElementById('population').innerHTML = population.toLocaleString('en');
             document.getElementById('update').innerHTML = update.substr(0, 10);
             document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
@@ -30,14 +31,15 @@ function getCovidStats() {
         .catch(function() {
             console.log("error");
         })
-	// update api data every 12 hours
+    // update every 12 hours
     setTimeout(getCovidStats, 43200000) 
 }
 
 
 
 // Second API - News from NY TIMES //
-//store url into variable
+
+//store important variables
 
 var headlines = document.getElementById("headlines");
 var endpoints = "https://api.nytimes.com/svc/topstories/v2/";
@@ -45,6 +47,7 @@ var topic = "world.json?";
 var key = "iGRIa7yg2feYJqwvCvLKKg9TDnJjAGLx";
 var url = url = `${endpoints}${topic}api-key=${key}`;
 
+//fetch data from nytimes api
 function nytimes() {
     fetch(url).then(response => response.json())
         .then(data => render(data))
@@ -57,12 +60,12 @@ function nytimes() {
             console.log(article.title);
 
             // creating the headlines div to have the top stories
-            let HeadlineDiv = $(`<div class="grid-item"  id="news-${headlines}">`);
-            let imgDiv = $(`<div class= 'image-overflow'>`);
-            let img = $(`<img class='rounded mx-auto d-block' src='${article.multimedia[0].url}'>`);
+            var HeadlineDiv = $(`<div class="grid-item"  id="news-${headlines}">`);
+            var imgDiv = $(`<div class= 'image-overflow'>`);
+            var img = $(`<img class='rounded mx-auto d-block' src='${article.multimedia[0].url}'>`);
             $(imgDiv).append(img);
-            let a = $("<a target='_blank'class= 'card-link' href=" + article.url + ">" + article.title + "</a></br>");
-            let p = $("<p class='card-text'  href=" + article.url + ">" + article.abstract + "</p></br>");
+            var a = $("<a target='_blank'class= 'card-link' href=" + article.url + ">" + article.title + "</a></br>");
+            var p = $("<p class='card-text'  href=" + article.url + ">" + article.abstract + "</p></br>");
 
             $(HeadlineDiv).append(a, img, p);
 
@@ -70,29 +73,3 @@ function nytimes() {
         });
     }
 };
-// var headlines = document.getElementById("headlines");
-// // url for fetching the top stories of nytimes
-// var url = "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=iGRIa7yg2feYJqwvCvLKKg9TDnJjAGLx";
-
-// fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-
-//         data.results.map(article => {
-//             console.log(article.title);
-
-//             // creating the headlines div to have the top stories
-//             let HeadlineDiv = $(`<div class="grid-item"  id="news-${headlines}">`);
-//             let imgDiv = $(`<div class= 'image-overflow'>`);
-//             let img = $(`<img class='rounded mx-auto d-block' src='${article.multimedia[0].url}'>`);
-//             $(imgDiv).append(img);
-//             let a = $("<a target='_blank'class= 'card-link' href=" + article.url + ">" + article.title + "</a></br>");
-//             let p = $("<p class='card-text'  href=" + article.url + ">" + article.abstract + "</p></br>");
-
-//             $(HeadlineDiv).append(a, img, p);
-
-//             $("#headlines").append(HeadlineDiv);
-//         })
-
-//     });
